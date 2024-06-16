fef22bc4cc875fdcc345bf2bcb76b6e0a50d5a31
gustavof04 <fernandes.gustavo2910@gmail.com>
2024-06-16 12:36:08 -0400

    feat: Add class-validator, general validations

--- src/users/domain/dtos/CreateUserDTO.ts
+++ src/users/domain/dtos/CreateUserDTO.ts
@@ -1,15 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsLowercase, IsString, Length } from 'class-validator';
 
 export class CreateUserDTO {
   readonly id?: string;
 
   @ApiProperty()
+  @IsString({ message: 'Nome inválido' })
+  @Length(3, 50, { message: 'Nome deve ter de 3 a 50 caracteres' })
   readonly name: string;
 
   @ApiProperty()
+  @IsEmail({}, { message: 'Email inválido' })
+  @IsLowercase({ message: 'Email não deve ter caracteres maiúsculos' })
   readonly email: string;
 
   @ApiProperty()
+  @IsString({ message: 'Senha inválida' })
+  @Length(8, 16, { message: 'Senha deve ter de 8 a 16 caracteres' })
   readonly password: string;
 
   readonly createdAt?: string;
